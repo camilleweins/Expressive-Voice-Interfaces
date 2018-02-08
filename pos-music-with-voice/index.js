606037c28180036170bbeac8d2fe7b150490cc34
Camille <camilleweins@gmail.com>
2018-02-08 02:51:26 -0500

    update

--- pos-music-with-voice/index.js
+++ pos-music-with-voice/index.js
@@ -3,18 +3,20 @@ var express = require('express');
 
 var app = express();
 
+app.set('port', (process.env.PORT || 5000));
+
 app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/index.html')
+  res.sendFile(__dirname + '/index.html');
 });
 
 app.get('/api/pos', function(req, res) {
 	var words = req.query.words;
-	var posWords = getPos(words)
-	res.send(posWords)
+	var posWords = getPos(words);
+	res.send(posWords);
 });
 
-app.use(express.static('public'))
+app.use(express.static('public'));
 
-app.listen(3000, function () {
+app.listen(app.get('port'), function () {
   console.log('Listening on port 3000!')
 });
